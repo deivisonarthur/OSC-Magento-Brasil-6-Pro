7bca00ac08c527a70682ec0ae3683a1fd375701a
Isaac Lopes <contato@isaaclopes.com.br>
2014-10-13 20:33:37 -0300

    ->retirado do validade jquery ->inclusao jquery por opção admin

--- js/onestepcheckout/validation.js
+++ js/onestepcheckout/validation.js
@@ -1,12 +1,3 @@
-if ((typeof jQuery === 'undefined') && !window.jQuery) {
-    document.write(unescape("%3Cscript type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'%3E%3C/script%3E"));
-} else {
-    if ((typeof jQuery === 'undefined') && window.jQuery) {
-        jQuery = window.jQuery;
-    } else if ((typeof jQuery !== 'undefined') && !window.jQuery) {
-        window.jQuery = jQuery;
-    }
-}
 jQuery.noConflict();
 function mascara(o, f) {
     v_obj = o
