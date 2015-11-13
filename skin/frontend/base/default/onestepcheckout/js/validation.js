e23649889f5541277bfb6e2481b423e3cdc7a918
Isaac Lopes <contato@isaaclopes.com.br>
2015-11-13 10:39:22 -0200

    cnpj

--- skin/frontend/base/default/onestepcheckout/js/validation.js
+++ skin/frontend/base/default/onestepcheckout/js/validation.js
@@ -21,6 +21,14 @@ function mdocumento(v) {
     }
     return v;
 }
+function mdocumentoCNPJ(v) {
+    v = v.replace(/\D/g, "");
+    v = v.replace(/^(\d{2})(\d)/, "$1.$2");
+    v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+    v = v.replace(/\.(\d{3})(\d)/, ".$1/$2");
+    v = v.replace(/(\d{4})(\d)/, "$1-$2");
+    return v;
+}
 function mdata(v) {
     v = v.replace(/\D/g, "")
     v = v.replace(/(\d{2})(\d)/, "$1/$2")
