95aa9c5e88844368ee6e341a780b5e9f301257cf
Eneias Ramos de Melo <eneias@gamuza.com.br>
2016-02-15 20:21:08 -0200

    Ajustado paths para requisições ajax

--- skin/frontend/base/default/onestepcheckout/js/validation.js
+++ skin/frontend/base/default/onestepcheckout/js/validation.js
@@ -147,7 +147,7 @@ function buscaCep(quale) {
        var cep = jQuery('input[name*="postcode"]').val();
         if (cep != '' && cep.length == 8) {
             loadposthideshow(true, '.onestepcheckout-postcod-process');
-            jQuery.getScript("/onestepcheckout/ajax/busca_cep?cep="+ cep + "", function() {
+            jQuery.getScript(BASE_URL + "onestepcheckout/ajax/busca_cep?cep="+ cep + "", function() {
                 loadposthideshow(false, '.onestepcheckout-postcod-process');
                 if (resultadoCEP["resultado"] != 0) {
                     if (unescape(resultadoCEP["logradouro"])) jQuery('input[name*="street[]"]').val(unescape(resultadoCEP["tipo_logradouro"]) + " "+ unescape(resultadoCEP["logradouro"]));
@@ -168,7 +168,7 @@ function buscaCep(quale) {
         var cep = jQuery('input[name*="' + quale + '[postcode]"]').val();
         if (cep != '' && cep.length == 8) {
             loadposthideshow(true, '.onestepcheckout-postcod-process');
-            jQuery.getScript("/onestepcheckout/ajax/busca_cep?cep="+ cep + "", function() {
+            jQuery.getScript(BASE_URL + "onestepcheckout/ajax/busca_cep?cep="+ cep + "", function() {
                 loadposthideshow(false, '.onestepcheckout-postcod-process');
                 if (resultadoCEP["resultado"] != 0) {
                     if (unescape(resultadoCEP["logradouro"])) jQuery('input[name*="' + quale + '[street][]"]').val(unescape(resultadoCEP["tipo_logradouro"]) + " "+ unescape(resultadoCEP["logradouro"]));
