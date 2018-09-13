3ece29a75ab1128a32331a42e1e6b6e8b8d1e7cf
Isaac Lopes <contato@isaaclopes.com.br>
2018-09-13 19:14:22 -0300

    atualiza

--- skin/frontend/base/default/onestepcheckout/js/validation.js
+++ skin/frontend/base/default/onestepcheckout/js/validation.js
@@ -1,4 +1,13 @@
 jQuery.noConflict();
+
+function mask(t, mask){
+    var i = t.value.length;
+    var saida = mask.substring(1,0);
+    var texto = mask.substring(i)
+    if (texto.substring(0,1) != saida){
+        t.value += texto.substring(0,1);
+    }
+}
 function mascara(o, f) {
     v_obj = o
     v_fun = f
